430478016ce0fb30c9d5d13b261488c4bd87489a
StKalinkin <89989831+StKalinkin@users.noreply.github.com>
2021-09-07 19:40:34 +0300

    'q'

---
+++ homework1/Hello.py
@@ -0,0 +1,29 @@
+#first task - Hello World!
+
+print('Hello World!')
+print(2 + 2)
+print(10/3)
+
+#second task - variables
+name = input("What's your name? ")
+print("Hi", name)
+
+#third task - simple type of data
+a = 2
+b = 0.5
+print(a + b)
+
+name = input("What's your name? ")
+print(f"hi, {name}.".capitalize())
+
+name = input('Введите Ваше имя: ').upper()
+print(f"Привет, {name}! Как дела?")
+
+v = int(input('Введите число от 1 до 10: '))
+print(v + 10)
+
+print(float('1'))
+print(int(2.5))
+print(bool('1'))
+print(bool(''))
+print(bool(0))
